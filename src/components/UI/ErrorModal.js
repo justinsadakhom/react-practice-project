c1f98505c118c767118791897635949d76b9391b
Justin Sadakhom <justin.sadakhom@flipp.com>
2022-07-25 14:09:11 -0400

    Add portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,26 @@
 import styled from "styled-components";
+import { Fragment } from "react";
+import { createPortal } from "react-dom";
 import { Button } from "./Button";
 import { Card } from "./Card";
 
-const StyledErrorModal = styled.div`
-  .backdrop {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    z-index: 10;
-    background: rgba(0, 0, 0, 0.75);
-  }
+const StyledBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background: rgba(0, 0, 0, 0.75);
+`;
 
-  .modal {
-    position: fixed;
-    top: 30vh;
-    left: 10%;
-    width: 80%;
-    z-index: 100;
-    overflow: hidden;
-  }
+const StyledCard = styled(Card)`
+  position: fixed;
+  top: 30vh;
+  left: 10%;
+  width: 80%;
+  z-index: 100;
+  overflow: hidden;
 
   .header {
     background: #4f005f;
@@ -50,21 +50,41 @@ const StyledErrorModal = styled.div`
   }
 `;
 
+const Backdrop = ({ onErrorClose }) => {
+  return <StyledBackdrop className="backdrop" onClick={onErrorClose} />;
+};
+
+const ModalOverlay = ({ title, message, onErrorClose }) => {
+  return (
+    <StyledCard className="modal">
+      <header className="header">
+        <h2>{title}</h2>
+      </header>
+      <div className="content">
+        <p>{message}</p>
+      </div>
+      <footer className="actions">
+        <Button onClick={onErrorClose}>Okay</Button>
+      </footer>
+    </StyledCard>
+  );
+};
+
 export const ErrorModal = ({ title, message, onErrorClose }) => {
   return (
-    <StyledErrorModal>
-      <div className="backdrop" onClick={onErrorClose} />
-      <Card className="modal">
-        <header className="header">
-          <h2>{title}</h2>
-        </header>
-        <div className="content">
-          <p>{message}</p>
-        </div>
-        <footer className="actions">
-          <Button onClick={onErrorClose}>Okay</Button>
-        </footer>
-      </Card>
-    </StyledErrorModal>
+    <Fragment>
+      {createPortal(
+        <Backdrop onErrorClose={onErrorClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onErrorClose={onErrorClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
